fa234b1cde5f2146ddbb7b0bbb57c5358cce11d0
ZSBI-Informatycy <claymorealt@op.pl>
2019-03-31 00:03:46 +0100

    Obsługa iframe'ów, style dla poddomen

--- public/js/main.js
+++ public/js/main.js
@@ -9,11 +9,14 @@
         return document.querySelector(what);
     }
 
+    // Get
+
     /* GLOBAL VALUES */
     /* ============= */
 
     // List of directories for use in file loading operations
-    const dirNames  = ['../css/', '../config/', '../web/', '../docs/'];
+    const dirNames  = ['css/', 'config/', 'web/', 'docs/'];
+    const dirUp = '../';
 
     // Version of the app
     const VERSION = "1.0.1";
@@ -25,7 +28,7 @@
     document.addEventListener('DOMContentLoaded', function() {
         // Linking the initial master file
         let linkStyle = function(style) {
-            let styleSheet = dirNames[0] + style + '.css';
+            let styleSheet = style + '.css';
             let styleSheetLink = document.createElement('link');
 
             // Set all the link paremeters
@@ -38,8 +41,35 @@
 
         /* MAIN INITIALIZATION */
         let init = function() {
-            linkStyle('master');
-            linkStyle('anims/main');
+            // Check the resolution for the mobile version
+            let width = window.screen.width * window.devicePixelRatio;
+
+            // Check if we're running in an iframe or is it the main page
+            if(window.self !== window.top) {
+                linkStyle(dirUp + dirUp + dirUp + 'css/master');
+                linkStyle(dirUp + dirUp + dirUp + 'css/anims/main');
+
+                if(width < 640) {
+                    linkStyle(dirUp + dirUp + dirUp + 'css/mobile/master');
+                }
+            } else {
+                linkStyle(dirUp + 'css/master');
+                linkStyle(dirUp + 'css/anims/main');
+
+                if(width < 640) {
+                    linkStyle(dirUp + 'css/mobile/master');
+                }
+            }
+
+            // Check the location of the document to load any additional css
+            let loc = window.location.pathname;
+            let dir = loc.substring(0, loc.lastIndexOf('/'));
+            console.log(dir);
+
+            // Regular Webpages
+            if(dir == "/web/main") {
+                linkStyle(dirUp + dirUp + dirUp + 'css/ui/web/primary');
+            }
 
             // Loader spinner
             setTimeout(function() {
@@ -48,7 +78,9 @@
             }, 3000);
 
             // Set the Version
-            $('#version').innerHTML = VERSION;
+            if($('#version')) {
+                $('#version').innerHTML = VERSION;
+            }
         }
 
         // Initialize with default CSS
